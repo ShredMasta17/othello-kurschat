967399d83421ded2d847e6766e4da08ff7d80d45
Marcus Kurschat <mkurschat@westmont.edu>
2022-05-30 16:06:48 -0700

    server.js working, partial index.html

--- server.js
+++ server.js
@@ -1,32 +1,52 @@
 /* ******** */
-/* Set up the static file server */
-let static = require('node-static');
 
-/* Set up the http server library */
-let http = require('http');
+const http = require('http');
 
-/* Assume that we are running on Heroku */
-let port = process.env.PORT;
-let directory = __dirname + '/public';
+const hostname = '127.0.0.1';
+const port = 3000;
 
-/* If we aren't on Heroku, then we need to adjust aour port and directory */
-if ((typeof port == 'undefined') || (port === null)) {
-    port = 8080;
-    directory = '/public';
-}
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
 
-/* Set up our sttic file web server to deliver files from the filesystem */
-let file = new static.Server(directory);
-
-/* Wiring it all together */
-let app = http.createServer(
-    function(request,response) {
-        request.addListener('end',
-            function() {
-                file.serve(request,response)
-            }
-        ).resume();
-    }
-).listen(port);
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 console.log('The server is running')
+
+
+/* Set up the static file server */
+// let static = require('node-static');
+
+// /* Set up the http server library */
+// let http = require('http');
+
+// /* Assume that we are running on Heroku */
+// let port = process.env.PORT;
+// let directory = __dirname + '/public';
+
+// /* If we aren't on Heroku, then we need to adjust aour port and directory */
+// if ((typeof port == 'undefined') || (port === null)) {
+//     port = 8080;
+//     directory = './public';
+// }
+
+// /* Set up our sttic file web server to deliver files from the filesystem */
+// let file = new static.Server(directory);
+
+// /* Wiring it all together */
+// let app = http.createServer(
+//     function(request,response) {
+//         request.addListener('end',
+//             function() {
+//                 file.serve(request,response)
+//             }
+//         ).resume();
+//     }
+// ).listen(port);
+
+
+// console.log('The server is running');
